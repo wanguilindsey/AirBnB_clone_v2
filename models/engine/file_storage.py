d4f3c81b8b9af233b53858411a74ccd4ed044f66
wanguilindsey <wanguilindsey@gmail.com>
2024-02-20 00:36:58 -0800

    delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,14 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is None:
             return self.__objects
         else:
             filtered_dict = {}
             for key, value in self.__objects.items():
-                if type(value) is cls:
+                if isinstance(value, cls):
                     filtered_dict[key] = value
             return filtered_dict
 
