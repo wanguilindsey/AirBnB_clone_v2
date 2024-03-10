499dabc2216a36482ef231720d9344f555f1e660
wanguilindsey <wanguilindsey@gmail.com>
2024-03-10 04:26:08 -0700

    UPDATE

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to the web servers using do_deploy
+Fabric script that distributes an archive to your web servers
 """
-
-from fabric.api import put, run, env
+from fabric.api import env, put, run
 from os.path import exists
-env.hosts = ['52.86.196.120', '100.24.235.246']
 
+env.hosts = ['52.86.196.120', '100.24.235.246']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """
+    Distributes an archive to the web servers and deploys it
+    """
+    if not exists(archive_path):
         return False
+
+    filename = archive_path.split('/')[-1]
+    no_extension = filename.split('.')[0]
+    release_path = f'/data/web_static/releases/{no_extension}'
+
     try:
-        archive_name = archive_path.split("/")[-1]
-        no_ext = archive_name.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_name, path, no_ext))
-        run('rm /tmp/{}'.format(archive_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        put(archive_path, '/tmp')
+        run(f'mkdir -p {release_path}')
+        run(f'tar -xzf /tmp/{filename} -C {release_path}')
+        run(f'rm /tmp/{filename}')
+        run(f'mv {release_path}/web_static/* {release_path}/')
+        run(f'rm -rf {release_path}/web_static')
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run(f'ln -s {release_path} /data/web_static/current')
+        print("New version deployed!")
         return True
     except Exception as e:
-        print("Error:", e)
+        print(e)
         return False
