73f40077f3cb268ea13918dd40707a7b91415f42
wanguilindsey <wanguilindsey@gmail.com>
2024-03-10 07:03:40 -0700

    puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -3,12 +3,11 @@ package { 'nginx':
   ensure => installed,
 }
 
-# Ensure the /data directory exists
+# Create necessary directories
 file { '/data':
   ensure => directory,
 }
 
-# Ensure the /data/web_static directory structure exists
 file { '/data/web_static':
   ensure => directory,
 }
@@ -25,26 +24,27 @@ file { '/data/web_static/releases/test':
   ensure => directory,
 }
 
-# html file for testing
+# Create a fake HTML file for testing
 file { '/data/web_static/releases/test/index.html':
-  content => '<html><head></head><body>Bernadette@alx</body></html>',
+  ensure  => file,
+  content => '<html><head></head><body>Holberton School</body></html>',
 }
 
-# Create a symbolic link to the latest release
+# Create a symbolic link if it doesn't exist
 file { '/data/web_static/current':
   ensure => link,
   target => '/data/web_static/releases/test',
   force  => true,
 }
 
-# Ensure ownership of the /data directory is set to ubuntu user and group recursively
+# Set ownership of /data folder to ubuntu user and group recursively
 file { '/data':
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
   recurse => true,
 }
 
-# Configure Nginx to serve the static content
+# Update Nginx configuration to serve content from /data/web_static/current
 file { '/etc/nginx/sites-available/default':
   ensure  => present,
   content => "
@@ -66,7 +66,7 @@ file { '/etc/nginx/sites-available/default':
   notify => Service['nginx'],
 }
 
-# Restart Nginx if the configuration changes
+# Restart Nginx service if the configuration changes
 service { 'nginx':
   ensure  => running,
   enable  => true,
