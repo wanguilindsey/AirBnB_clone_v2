4e09d21f931938a2855ef26e58f05d408bae7838
wanguilindsey <wanguilindsey@gmail.com>
2024-03-10 05:18:49 -0700

    updates

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,9 +4,10 @@ Fabric script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers using the function deploy
 """
 
-from fabric.api import env, local, put, run
+from fabric.api import env
+from fabric.operations import local, put, run
 from datetime import datetime
-from os.path import exists, isdir
+from os.path import exists, isdir, basename
 env.hosts = ['52.86.196.120', '100.24.235.246']
 
 
@@ -25,21 +26,23 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Distributes an archive to the web servers"""
+    if not exists(archive_path):
         return False
+
     try:
-        archive_name = archive_path.split("/")[-1]
+        archive_name = basename(archive_path)
         no_ext = archive_name.split(".")[0]
-        path = "/data/web_static/releases/"
+        release_path = "/data/web_static/releases/"
+
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_name, path, no_ext))
+        run('mkdir -p {}{}/'.format(release_path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_name, release_path, no_ext))
         run('rm /tmp/{}'.format(archive_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(release_path, no_ext))
+        run('rm -rf {0}{1}/web_static'.format(release_path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(release_path, no_ext))
         return True
     except Exception as e:
         print("Error:", e)
