ff459ccf13477aebb8151536089af1d5b1b0b3d0
wanguilindsey <wanguilindsey@gmail.com>
2024-03-10 04:33:27 -0700

    updates

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
+Fabric script that distributes an archive to the web servers using do_deploy
 """
-from fabric.api import env, put, run
+
+from fabric.api import put, run, env
 from os.path import exists
 
 env.hosts = ['52.86.196.120', '100.24.235.246']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to the web servers and deploys it
-    """
+    """Distributes an archive to the web servers"""
     if not exists(archive_path):
         return False
 
-    filename = archive_path.split('/')[-1]
-    no_extension = filename.split('.')[0]
-    release_path = f'/data/web_static/releases/{no_extension}'
-
     try:
-        put(archive_path, '/tmp')
-        run(f'mkdir -p {release_path}')
-        run(f'tar -xzf /tmp/{filename} -C {release_path}')
-        run(f'rm /tmp/{filename}')
-        run(f'mv {release_path}/web_static/* {release_path}/')
-        run(f'rm -rf {release_path}/web_static')
-        run('rm -rf /data/web_static/current')
-        run(f'ln -s {release_path} /data/web_static/current')
-        print("New version deployed!")
+        archive_name = archive_path.split("/")[-1]
+        no_ext = archive_name.split(".")[0]
+        release_path = "/data/web_static/releases/"
+
+        put(archive_path, '/tmp/')
+        run(f'mkdir -p {release_path}{no_ext}/')
+        run(f'tar -xzf /tmp/{archive_name} -C {release_path}{no_ext}/')
+        run(f'rm /tmp/{archive_name}')
+        run(f'mv {release_path}{no_ext}/web_static/* {release_path}{no_ext}/')
+        run(f'rm -rf {release_path}{no_ext}/web_static')
+        run(f'rm -rf /data/web_static/current')
+        run(f'ln -s {release_path}{no_ext}/ /data/web_static/current')
         return True
     except Exception as e:
-        print(e)
+        print(f"Error deploying archive: {e}")
         return False
